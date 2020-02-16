42a8e8119cfa9c0aba84599176b6f1bb80d73999
Neil Macneale <neil.macneale.v@gmail.com>
2020-02-16 08:43:51 -0800

    Loaded correct data into arrays

--- src/opengl/loader.cpp
+++ src/opengl/loader.cpp
@@ -76,14 +76,15 @@ GLuint loadShaderProgram(string vertexFilename, string fragmentFilename) {
 
 bool loadOBJ (
   const char* path,
-  std::vector <glm::vec3>& out_vertices,
-  std::vector <glm::vec2>& out_uvs,
-  std::vector <glm::vec3>& out_normals
+  std::vector<uint>& out_indices,
+  std::vector<glm::vec3>& out_vertices,
+  std::vector<glm::vec2>& out_uvs,
+  std::vector<glm::vec3>& out_normals
 ) {
-  std::vector <unsigned int> vertexIndices, uvIndices, normalIndices;
-  std::vector <glm::vec3> temp_vertices;
-  std::vector <glm::vec2> temp_uvs;
-  std::vector <glm::vec3> temp_normals;
+  std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
+  std::vector<glm::vec3> vertices;
+  std::vector<glm::vec2> uvs;
+  std::vector<glm::vec3> normals;
   FILE* file = fopen(path, "r");
   if (file == NULL) {
     cout << "Failed to open the file !" << endl;
@@ -98,15 +99,15 @@ bool loadOBJ (
     if (strcmp(lineHeader, "v") == 0) {
       glm::vec3 vertex;
       fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
-      temp_vertices.push_back(vertex);
+      vertices.push_back(vertex);
     } else if (strcmp(lineHeader, "vt") == 0) {
       glm::vec2 uv;
       fscanf(file, "%f %f\n", &uv.x, &uv.y );
-      temp_uvs.push_back(uv);
+      uvs.push_back(uv);
     } else if (strcmp(lineHeader, "vn") == 0) {
       glm::vec3 normal;
       fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
-      temp_normals.push_back(normal);
+      normals.push_back(normal);
     } else if (strcmp(lineHeader, "f") == 0) {
       unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
       int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
