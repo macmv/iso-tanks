b7b816b3917a112699234bfc441de18eb62f951f
Neil Macneale <neil.macneale.v@gmail.com>
2020-02-16 08:44:33 -0800

    Headers

--- src/opengl/loader.h
+++ src/opengl/loader.h
@@ -12,9 +12,18 @@ GLuint loadShaderProgram(string vertexFilename, string fragmentFilename);
 
 bool loadOBJ(
   const char* path,
+  std::vector<unsigned int>& out_indices,
   std::vector < glm::vec3 > & out_vertices,
   std::vector < glm::vec2 > & out_uvs,
   std::vector < glm::vec3 > & out_normals
 );
+
+GLuint createVAO(
+  std::vector <unsigned int>& indices,
+  std::vector <glm::vec3>& vertices,
+  std::vector <glm::vec2>& uvs,
+  std::vector <glm::vec3>& normals
+);
+
 
 #endif
