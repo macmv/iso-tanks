303464d53b540bcf8904805a841726120f8b065a
Neil Macneale <neil.macneale.v@gmail.com>
2020-03-04 00:57:29 -0800

    Made speed and torque based on current velocity, so you can't reach insane speeds

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -21,14 +21,17 @@ void Player::update() {
   body->getMotionState()->getWorldTransform(transform);
   transform.getOpenGLMatrix(glm::value_ptr(scene->transform));
 
-  float speed = .20f;
-  float torqueSpeed = 5;
-
   float torqueAmount = 0;
   glm::vec3 force = glm::vec3(0, 0, 0);
   glm::vec3 up = glm::normalize(glm::vec3(scene->transform[3]) * -1.f);
   glm::vec3 forward = glm::vec3(scene->transform * glm::vec4(0, 0, 1, 0));
   glm::vec3 left = cross(up, forward);
+
+  glm::vec3 vel = glm::vec3(body->getLinearVelocity().x(), body->getLinearVelocity().y(), body->getLinearVelocity().z());
+  glm::vec3 angVel = glm::vec3(body->getAngularVelocity().x(), body->getAngularVelocity().y(), body->getAngularVelocity().z());
+  float speed = .8f - glm::length(vel) / 10;
+  float torqueSpeed = 0.1f - glm::length(angVel * up) / 10;
+
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
     force += forward * speed;
   }
@@ -52,7 +55,7 @@ void Player::update() {
   body->applyCentralImpulse(btVector3(force.x, force.y, force.z));
 
   glm::vec3 torqueForce = up * torqueAmount;
-  body->applyTorque(btVector3(torqueForce.x, torqueForce.y, torqueForce.z));
+  body->applyTorqueImpulse(btVector3(torqueForce.x, torqueForce.y, torqueForce.z));
 }
 
 glm::mat4 Player::getTransform() {
