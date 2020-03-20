1acb6aa7acf56db8cf8c4704dccce2835fa66867
Neil Macneale <neil.macneale.v@gmail.com>
2020-03-20 14:13:07 -0700

    Created a function that converts a player to a player proto

--- src/network/proto_util.cpp
+++ src/network/proto_util.cpp
@@ -2,11 +2,26 @@
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtx/quaternion.hpp>
+#include "player/player.h"
 
 glm::mat4 ProtoUtil::to_glm(Transform trans) {
   glm::mat4 transform = glm::mat4(1);
   transform = glm::translate(transform, glm::vec3(trans.position().x(), trans.position().y(), trans.position().z()));
   transform = transform * glm::toMat4(glm::quat(trans.rotation().w(), trans.rotation().x(), trans.rotation().y(), trans.rotation().z()));
   return transform;
+}
+
+void ProtoUtil::to_proto(PlayerProto* proto, uint id, Player* player) {
+  glm::vec3 position = glm::vec3(player->getTransform()[3]);
+  glm::quat rotation = glm::quat_cast(player->getTransform());
+
+  proto->mutable_transform()->mutable_position()->set_x(position.x);
+  proto->mutable_transform()->mutable_position()->set_y(position.y);
+  proto->mutable_transform()->mutable_position()->set_z(position.z);
+  proto->mutable_transform()->mutable_rotation()->set_x(rotation.x);
+  proto->mutable_transform()->mutable_rotation()->set_y(rotation.y);
+  proto->mutable_transform()->mutable_rotation()->set_z(rotation.z);
+  proto->mutable_transform()->mutable_rotation()->set_w(rotation.w);
+  proto->set_id(id);
 }
 
--- src/network/proto_util.h
+++ src/network/proto_util.h
@@ -3,10 +3,12 @@
 
 #include "src/proto/multiplayer.grpc.pb.h"
 #include <glm/glm.hpp>
+#include "player/player.h"
 
 class ProtoUtil {
   public:
     static glm::mat4 to_glm(Transform trans);
+    static void to_proto(PlayerProto* proto, uint id, Player* player);
 };
 
 #endif
